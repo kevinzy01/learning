fe792710507c5455d655dc7d980461931f03f471
Kevin <kevinzy01@gmail.com>
2018-03-25 20:25:28 +0200

    Weather App

---
+++ weatherApp/index.js
@@ -0,0 +1,16 @@
+const request = require('request');
+const argv = require('yargs').argv;
+
+let apiKey = "bc020bd3ee69505799f67deb1e4808a6";
+let city = argv.c || "vinaros";
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+request(url, function (err, res, body) {
+  if (err) {
+    console.log("err:", err);
+  } else {
+    let weather = JSON.parse(body);
+    let temp = `It's ${weather.main.temp} degrees, there's an humidity of ${weather.main.humidity}% and a wind speed of ${weather.wind.speed} in ${weather.name}, ${weather.sys.country}`;
+    console.log(temp);
+  }
+});
